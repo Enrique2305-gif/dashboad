dd2b43b2620379f88f3325f0bf268f00b3b3120f
Enrique2305-gif <egrosado@espol.edu.ec>
2025-12-08 19:36:14 +0000

    parte guia16

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import AlertUI from './components/AlertUI';
 import SelectorUI from './components/SelectorUI'
 import IndicatorUI from './components/IndicatorUI';
 import useFetchData from './functions/useFetchData';
+import TableUI from './components/TableUI';
+import ChartUI from './components/ChartUI';
 
 
 function App() {
@@ -85,10 +87,14 @@ function App() {
       </Grid> 
 
       {/* Gráfico */}
-      <Grid sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
+      <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>
+      <ChartUI />
+      </Grid>
 
-      {/* Tabla */}
-      <Grid sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
+     {/* Tabla */}
+     <Grid size={{ xs: 6, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>
+     <TableUI />
+     </Grid>
 
       {/* Información adicional */}
       <Grid>Elemento: Información adicional</Grid>
